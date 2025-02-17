be6b550c007813fefdefa5525353395ff20c4d51
Mashno <118561914+Mashno@users.noreply.github.com>
2025-02-17 17:26:12 +0300

    GUI+methods
    добавлены методы add , substract
    Создана кнопка для введения двух чисел и запуска метода add

---
+++ Fp/src/fp/CalculatorGUI.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fp;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+/**
+ *
+ * @author Владислав
+ */
+public class CalculatorGUI extends JFrame implements ActionListener{
+     JPanel panel;
+     JButton button;
+     JTextField f1, f2;
+     JLabel label;
+    
+    public CalculatorGUI(){
+        super("меню для использования гуи");
+        panel = new JPanel();
+        getContentPane().add(panel);
+        
+        button = new JButton("нажмите для сложения");
+        f1 = new JTextField(30);
+        f2 = new JTextField(30);
+        label = new JLabel("здесь будет результат");
+        
+        button.setActionCommand("Ok");
+        button.addActionListener(this);
+        panel.add(button);
+        panel.add(f1);
+        panel.add(f2);
+        panel.add(label);
+        
+        pack();
+        setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String str = e.getActionCommand();
+        if(str.equals("Ok")){
+            double a = Double.parseDouble(f1.getText());
+            double b = Double.parseDouble(f2.getText());
+            label.setText(Double.toString(Calculator.add(a, b)));
+        }
+    }
+}
