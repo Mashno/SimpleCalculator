0b9681c422b2e647388fbb7cc4d380197f5ab2bf
moneyluv <ababaan365@gmail.com>
2025-02-17 17:49:53 +0300

    new feature
    bububu

---
+++ Fp/src/fp/Calculator.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fp;
+
+/**
+ *
+ * @author Владислав
+ */
+public class Calculator {
+    public static double add(double a, double b){
+        return a+b;
+    }
+    
+    public static double substract(double a, double b){
+        return a-b;
+    }
+    public static double multiply(double a, double b){
+        return a*b;
+    }
+    public static double divide(double a, double b){
+        return a/b;
+    }
+}
